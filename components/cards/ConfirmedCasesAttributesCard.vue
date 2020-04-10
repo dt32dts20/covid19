4d53875784ade9c486f44e71a217298457bc009f
Daiki <nojiri1098@gmail.com>
2020-04-10 18:28:43 +0900

    Using API in ConfirmedCasesAttributesCard

--- components/cards/ConfirmedCasesAttributesCard.vue
+++ components/cards/ConfirmedCasesAttributesCard.vue
@@ -5,7 +5,7 @@
       :title-id="'attributes-of-confirmed-cases'"
       :chart-data="patientsTable"
       :chart-option="{}"
-      :date="Data.patients.date"
+      :date="date"
       :info="sumInfoOfPatients"
       :url="'https://data.bodik.jp/dataset/_covid19'"
     />
@@ -13,7 +13,6 @@
 </template>
 
 <script>
-import Data from '@/data/json/data.json'
 import formatGraph from '@/utils/formatGraph'
 import formatTable from '@/utils/formatTable'
 import DataTable from '@/components/DataTable.vue'
@@ -23,46 +22,55 @@ export default {
     DataTable
   },
   data() {
-    // 感染者数グラフ
-    const patientsGraph = formatGraph(Data.patients_summary.data)
-    // 感染者数
-    const patientsTable = formatTable(Data.patients.data)
-
-    const sumInfoOfPatients = {
-      lText: patientsGraph[
-        patientsGraph.length - 1
-      ].cumulative.toLocaleString(),
-      sText: this.$t('{date}の累計', {
-        date: patientsGraph[patientsGraph.length - 1].label
-      }),
-      unit: this.$t('人')
+    return {
+      date: String,
+      patientsTable: {},
+      sumInfoOfPatients: {}
     }
-
-    // 陽性患者の属性 ヘッダー翻訳
-    for (const header of patientsTable.headers) {
-      header.text =
-        header.value === '退院' ? this.$t('退院※') : this.$t(header.value)
+  },
+  created() {
+    this.setDataUsingAPI()
+  },
+  methods: {
+    async setDataUsingAPI() {
+      await this.$axios
+        .get('https://data-covid19-oita.netlify.com/json/data.json')
+        .then(response => {
+          const json = response.data
+          this.date = json.patients.date
+          this.patientsTable = formatTable(json.patients.data)
+          const patientsGraph = formatGraph(json.patients_summary.data)
+          this.sumInfoOfPatients = {
+            lText: patientsGraph[
+              patientsGraph.length - 1
+            ].cumulative.toLocaleString(),
+            sText: this.$t('{date}の累計', {
+              date: patientsGraph[patientsGraph.length - 1].label
+            }),
+            unit: this.$t('人')
+          }
+          // 陽性患者の属性 ヘッダー翻訳
+          for (const header of this.patientsTable.headers) {
+            header.text =
+              header.value === '退院' ? this.$t('退院※') : this.$t(header.value)
+          }
+          // 陽性患者の属性 中身の翻訳
+          for (const row of this.patientsTable.datasets) {
+            row['居住地'] = this.$t(row['居住地'])
+            row['性別'] = this.$t(row['性別'])
+            row['退院'] = this.$t(row['退院'])
+            if (row['年代'] === '10歳未満') {
+              row['年代'] = this.$t('10歳未満')
+            } else {
+              const age = row['年代'].substring(0, 2)
+              row['年代'] = this.$t('{age}代', { age })
+            }
+          }
+        })
+        .catch(error => {
+          this.date = error
+        })
     }
-    // 陽性患者の属性 中身の翻訳
-    for (const row of patientsTable.datasets) {
-      row['居住地'] = this.$t(row['居住地'])
-      row['性別'] = this.$t(row['性別'])
-      row['退院'] = this.$t(row['退院'])
-
-      if (row['年代'] === '10歳未満') {
-        row['年代'] = this.$t('10歳未満')
-      } else {
-        const age = row['年代'].substring(0, 2)
-        row['年代'] = this.$t('{age}代', { age })
-      }
-    }
-
-    const data = {
-      Data,
-      patientsTable,
-      sumInfoOfPatients
-    }
-    return data
   }
 }
 </script>
