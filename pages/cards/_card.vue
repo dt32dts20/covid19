6beb66e2310df55cbe14f5ca02fa8f1456902f9f
Daiki <nojiri1098@gmail.com>
2020-04-16 13:16:17 +0900

    Remove import releted data.json

--- pages/cards/_card.vue
+++ pages/cards/_card.vue
@@ -12,12 +12,6 @@
     <tested-number-card
       v-else-if="this.$route.params.card == 'number-of-tested'"
     />
-    <telephone-advisory-reports-number-card
-      v-else-if="
-        this.$route.params.card ==
-          'number-of-reports-to-covid19-telephone-advisory-center'
-      "
-    />
     <consultation-desk-reports-number-card
       v-else-if="
         this.$route.params.card ==
@@ -28,12 +22,10 @@
 </template>
 
 <script>
-import Data from '@/data/json/data.json'
 import ConfirmedCasesDetailsCard from '@/components/cards/ConfirmedCasesDetailsCard.vue'
 import ConfirmedCasesNumberCard from '@/components/cards/ConfirmedCasesNumberCard.vue'
 import ConfirmedCasesAttributesCard from '@/components/cards/ConfirmedCasesAttributesCard.vue'
 import TestedNumberCard from '@/components/cards/TestedNumberCard.vue'
-import TelephoneAdvisoryReportsNumberCard from '@/components/cards/TelephoneAdvisoryReportsNumberCard.vue'
 import ConsultationDeskReportsNumberCard from '@/components/cards/ConsultationDeskReportsNumberCard.vue'
 
 export default {
@@ -42,41 +34,30 @@ export default {
     ConfirmedCasesNumberCard,
     ConfirmedCasesAttributesCard,
     TestedNumberCard,
-    TelephoneAdvisoryReportsNumberCard,
     ConsultationDeskReportsNumberCard
   },
   data() {
-    let title, updatedAt
+    let title
     switch (this.$route.params.card) {
       case 'details-of-confirmed-cases':
         title = this.$t('検査陽性者の状況')
-        updatedAt = Data.inspections_summary.date
         break
       case 'number-of-confirmed-cases':
         title = this.$t('陽性患者数')
-        updatedAt = Data.patients.date
         break
       case 'attributes-of-confirmed-cases':
         title = this.$t('陽性患者の属性')
-        updatedAt = Data.patients.date
         break
       case 'number-of-tested':
         title = this.$t('検査実施数')
-        updatedAt = Data.inspections_summary.date
-        break
-      case 'number-of-reports-to-covid19-telephone-advisory-center':
-        title = this.$t('新型コロナコールセンター相談件数')
-        updatedAt = Data.contacts.date
         break
       case 'number-of-reports-to-covid19-consultation-desk':
         title = this.$t('相談窓口相談件数')
-        updatedAt = Data.querents.date
         break
     }
 
     const data = {
-      title,
-      updatedAt
+      title
     }
     return data
   },
@@ -87,7 +68,7 @@ export default {
       this.$i18n.locale === 'ja'
         ? `${url}/ogp/${this.$route.params.card}.png?t=${timestamp}`
         : `${url}/ogp/${this.$i18n.locale}/${this.$route.params.card}.png?t=${timestamp}`
-    const description = `${this.updatedAt} | ${this.$t(
+    const description = `${this.$t(
       '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、有志が開設したものです。'
     )}`
 
