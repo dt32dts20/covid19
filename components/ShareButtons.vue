00ac9193169c27f90fa4fd6e31e278fdb64443fb
Daiki <nojiri1098@gmail.com>
2020-05-14 22:07:26 +0900

    Add ShareButtons component

---
+++ components/ShareButtons.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <a
+      href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+      class="twitter-share-button"
+      data-show-count="false"
+      >Tweet</a
+    >
+    <iframe
+      src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Foita.stopcovid19.jp%2F&layout=button_count&size=small&appId=113749536134548&width=100&height=20"
+      width="100"
+      height="20"
+      style="border:none;overflow:hidden"
+      scrolling="no"
+      frameborder="0"
+      allowtransparency="true"
+      allow="encrypted-media"
+    />
+    <script
+      async
+      src="https://platform.twitter.com/widgets.js"
+      charset="utf-8"
+    />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,7 @@
 <template>
   <div class="MainPage">
     <page-header :icon="headerItem.icon" :title="headerItem.title" />
-    <div>
-      <a
-        href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-        class="twitter-share-button"
-        data-show-count="false"
-        >Tweet</a
-      >
-      <iframe
-        src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Foita.stopcovid19.jp%2F&layout=button_count&size=small&appId=113749536134548&width=100&height=20"
-        width="100"
-        height="20"
-        style="border:none;overflow:hidden"
-        scrolling="no"
-        frameborder="0"
-        allowTransparency="true"
-        allow="encrypted-media"
-      />
-      <script
-        async
-        src="https://platform.twitter.com/widgets.js"
-        charset="utf-8"
-      />
-    </div>
+    <share-buttons />
     <whats-new :items="newsItems" />
     <card-row class="DataBlock">
       <patients-breakdown-card />
@@ -42,6 +20,7 @@
 import Vue from 'vue'
 import { MetaInfo } from 'vue-meta'
 import PageHeader from '@/components/PageHeader.vue'
+import ShareButtons from '@/components/ShareButtons.vue'
 import WhatsNew from '@/components/WhatsNew.vue'
 import CardRow from '@/components/cards/CardRow.vue'
 import PatientsBreakdownCard from '@/components/cards/PatientsBreakdownCard.vue'
@@ -56,6 +35,7 @@ import ConsultationsTransitionCard from '@/components/cards/ConsultationsTransit
 export default Vue.extend({
   components: {
     PageHeader,
+    ShareButtons,
     WhatsNew,
     CardRow,
     PatientsBreakdownCard,
