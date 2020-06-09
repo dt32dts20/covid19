e66e3b15824d9f443a6995a151b1b584781f9e7c
Daiki <nojiri1098@gmail.com>
2020-06-09 22:33:33 +0900

    Update formatter for UsageStatsByIndustry

--- components/cards/support/UsageStatsByIndustryCard.vue
+++ components/cards/support/UsageStatsByIndustryCard.vue
@@ -5,7 +5,7 @@
         $t('新型コロナウイルス関連の大分県における融資の利用状況 (業種別件数)')
       "
       :title-id="'usage-stats-by-industry'"
-      :chart-data="ageGraph"
+      :chart-data="chartData"
       :date="date"
       :unit="$t('件.tested')"
       :info="$t('累計値')"
@@ -27,7 +27,7 @@ export default {
   data() {
     return {
       date: String,
-      ageGraph: []
+      chartData: []
     }
   },
   created() {
@@ -36,19 +36,25 @@ export default {
   methods: {
     setData() {
       // const json = this.$store.state.data
-      // this.ageGraph = formatVariableGraph(json.age.data)
-      this.ageGraph = formatVariableGraph({
-        飲食業: 189,
-        'サービス業 (宿泊除く)': 167,
-        小売業: 154,
-        卸売業: 132,
-        建設業: 98,
-        宿泊業: 88,
-        製造業: 64,
-        その他: 12
-      })
-      // this.date = json.patients_summary.date
-      this.date = '2020/06/06 18:09'
+      const json = {
+        loan_achivements_by_industry: {
+          date: '2020/06/03 19:09',
+          data: {
+            飲食店: 100,
+            'サービス業 (宿泊除く)': 3,
+            小売業: 13,
+            卸売業: 6,
+            建設業: 9,
+            宿泊業: 10,
+            その他: 4
+          }
+        }
+      }
+
+      this.chartData = formatVariableGraph(
+        json.loan_achivements_by_industry.data
+      )
+      this.date = json.loan_achivements_by_industry.date
     }
   }
 }
